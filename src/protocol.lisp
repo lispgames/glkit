20745c5b439e8e86de727c7ae3e40e7e7ee695bc
Ryan Pavlik <rpavlik@gmail.com>
2014-12-12 16:04:28 -0600

    Allow vectors and lists of objects to be passed to GL-DELETE(-OBJECT)

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -6,6 +6,12 @@
   (:documentation "Call the appropriate gl* function to free the resource
 `GL-OBJECT`."))
 
+(defmethod gl-delete-object ((gl-objects list))
+  (map nil #'gl-delete-object gl-objects))
+
+(defmethod gl-delete-object ((gl-objects vector))
+  (map nil #'gl-delete-object gl-objects))
+
 (defun gl-delete (&rest gl-objects)
   "Delete `GL-OBJECTS` by calling `GL-DELETE-OBJECT` on each."
   (map nil #'gl-delete-object gl-objects))
