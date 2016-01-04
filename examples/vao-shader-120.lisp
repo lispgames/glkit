5738e102a1a2d10ac143ddf458603374f3640d85
Ryan Pavlik <rpavlik@gmail.com>
2016-01-04 12:37:35 -0600

    Fix erroneous GLSL 120

--- examples/vao-shader-120.lisp
+++ examples/vao-shader-120.lisp
@@ -20,8 +20,8 @@
 
 uniform mat4 view_m;
 
-in vec2 vertex;
-in vec3 color;
+attribute vec2 vertex;
+attribute vec3 color;
 
 varying vec3 f_color;
 
@@ -33,14 +33,13 @@ void main() {
            (:fragment-shader "
 #version 120
 
-in vec3 f_color;
+varying vec3 f_color;
 
 void main() {
     gl_FragColor = vec4(f_color, 1.0);
 }
 ")))
 
-#-darwin
 (defun vao-shader-120 ()
   (kit.sdl2:start)
   (sdl2:in-main-thread ()
@@ -48,9 +47,5 @@ void main() {
     (sdl2:gl-set-attr :context-minor-version 1)
     (sdl2:gl-set-attr :context-profile-mask 1))
   (make-instance 'vao-shader-window :shaders 'vao-color.programs.120))
-
-#+darwin
-(defun vao-shader-120 ()
-  (format t "This example does not work on darwin. Try running (kit.gl.test:vao-shader-410)."))
 
 ;; (kit.gl.test:vao-shader-120)
