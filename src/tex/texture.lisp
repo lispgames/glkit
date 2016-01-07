b2f8697261ea8f3f842d608fa8dfe9d328aaf936
Ryan Pavlik <rpavlik@gmail.com>
2016-01-06 18:29:23 -0600

    Replace WIDTH/HEIGHT/DEPTH with a VEC3; remove spurious debugging

--- src/tex/texture.lisp
+++ src/tex/texture.lisp
@@ -3,9 +3,7 @@
 (defclass texture ()
   ((id :initform (car (gl:gen-textures 1)) :initarg :id :accessor texture-id)
    (target :initform :texture-2d :initarg :target :accessor texture-target)
-   (width :initarg :width :initform nil :accessor texture-width)
-   (height :initarg :height :initform nil :accessor texture-height)
-   (depth :initarg :depth :initform nil :accessor texture-depth)))
+   (size :initarg :size :initform nil :accessor texture-size :type kit.glm:vec3)))
 
 (defgeneric tex-parameters (tex &key &allow-other-keys)
   (:documentation "Set texture parameters for `TEX`, bound to
@@ -48,51 +46,54 @@ Note that creating a texture causes it to become bound."))
   (with-slots (id) tex
     (gl:delete-textures (list id))))
 
-(define-tex-fun tex-image-1d (target width)
+(define-tex-fun tex-image-1d (target size)
     ((border 0) (level 0) (internal-format :rgba) (format :rgba)
      (type :unsigned-int-8-8-8-8) data)
-  (gl:tex-image-1d target level internal-format width border
+  (gl:tex-image-1d target level internal-format (aref size 0) border
                    format type (or data (cffi-sys:null-pointer))))
 
 (define-tex-fun tex-sub-image-1d (target)
-    ((level 0) (xoffset 0) width (format :rgba) (type :unsigned-int-8-8-8-8)
+    ((level 0) (xoffset 0) size (format :rgba) (type :unsigned-int-8-8-8-8)
      data)
-  (gl:tex-sub-image-1d target level xoffset width format type
+  (gl:tex-sub-image-1d target level xoffset (aref size 0) format type
                        (or data (cffi-sys:null-pointer))))
 
-(define-tex-fun tex-image-2d (target width height)
+(define-tex-fun tex-image-2d (target size)
     ((border 0) (level 0) (internal-format :rgba) (format :rgba)
      (type :unsigned-int-8-8-8-8) data)
-  (:say target width height data)
-  (gl:tex-image-2d target level internal-format width height border
+  (gl:tex-image-2d target level internal-format
+                   (aref size 0) (aref size 1) border
                    format type (or data (cffi-sys:null-pointer))))
 
 (define-tex-fun tex-sub-image-2d (target)
-    ((level 0) (xoffset 0) (yoffset 0) width height (format :rgba)
+    ((level 0) (xoffset 0) (yoffset 0) size (format :rgba)
      (type :unsigned-int-8-8-8-8) data)
-  (gl:tex-sub-image-2d target level xoffset yoffset width height
+  (gl:tex-sub-image-2d target level xoffset yoffset
+                       (aref size 0) (aref size 1)
                        format type (or data (cffi-sys:null-pointer))))
 
-(define-tex-fun tex-image-2d-multisample (target width height)
+(define-tex-fun tex-image-2d-multisample (target size)
     (samples (internal-format :rgba) fixed-sample-locations)
-  (%gl:tex-image-2d-multisample target samples internal-format width height
+  (%gl:tex-image-2d-multisample target samples internal-format
+                                (aref size 0) (aref size 1)
                                 fixed-sample-locations))
 
-(define-tex-fun tex-image-3d (target width height depth)
+(define-tex-fun tex-image-3d (target size)
     ((border 0) (level 0) (internal-format :rgba) (format :rgba)
      (type :unsigned-int-8-8-8-8) data)
-  (gl:tex-image-3d target level internal-format width height depth border
+  (gl:tex-image-3d target level internal-format
+                   (aref size 0) (aref size 1) (aref size 2) border
                    format type (or data (cffi-sys:null-pointer))))
 
 (define-tex-fun tex-sub-image-3d (target)
-    ((level 0) (xoffset 0) (yoffset 0) (zoffset 0) width height depth
+    ((level 0) (xoffset 0) (yoffset 0) (zoffset 0) size
      (format :rgba) (type :unsigned-int-8-8-8-8) data)
   (gl:tex-sub-image-3d target level xoffset yoffset zoffset
-                       width height depth
+                       (aref size 0) (aref size 1) (aref size 2)
                        format type (or data (cffi-sys:null-pointer))))
 
-(define-tex-fun tex-image-3d-multisample (target width height depth)
+(define-tex-fun tex-image-3d-multisample (target size)
     (samples (internal-format :rgba) fixed-sample-locations)
   (%gl:tex-image-3d-multisample target samples internal-format
-                                width height depth
+                                (aref size 0) (aref size 1) (aref size 2)
                                 fixed-sample-locations))
